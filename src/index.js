0e51026aa713588c7b364c5e1f7bb1b6625679de
James Logue <logueja@gmail.com>
2018-02-05 16:02:51 -0700

    Install redux

--- src/index.js
+++ src/index.js
@@ -4,10 +4,20 @@ import './index.css';
 import App from './containers/App/App';
 import { BrowserRouter } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const middleware = applyMiddleware(logger);
+const store = createStore(devTools, middleware);
+
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>, 
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter> 
+  </Provider>,
   document.getElementById('root'));
 registerServiceWorker();
