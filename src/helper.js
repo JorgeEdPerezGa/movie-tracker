38fd4415b44f503d453031754f87a8c9e1119b3d
Amanda Tjan <amanda.tjan@gmail.com>
2018-02-08 17:13:55 -0700

    Change key names in data cleaner function

--- src/helper.js
+++ src/helper.js
@@ -4,12 +4,13 @@ export const initialFetch = async() => {
   const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${key}`;
   const initialFetch = await fetch(url);
   const response = await initialFetch.json();
+  console.log(response)
   return cleanMovies(response);
 };
 
 export const cleanMovies = (response) => {
-  const movies = response.results.map(({ title, overview, poster_path, backdrop_path, id }) => {
-    return { title, overview, poster: poster_path, backdrop: backdrop_path, id };
+  const movies = response.results.map(({ title, overview, poster_path, backdrop_path, id, vote_average }) => {
+    return { title, overview, poster_path, backdrop_path, id, vote_average };
   });
   return Promise.all(movies);
 };
@@ -42,6 +43,23 @@ export const loginUser = async ({ email, password }) => {
       }
     });
     return await response.json();
+  } catch (error) {
+    return false;
+  }
+};
+
+export const addFavorite = async user => {
+  try {
+    const url='/api/users/new';
+    const register = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(user)
+    });
+
+    return await register.json();
   } catch (error) {
     return false;
   }
