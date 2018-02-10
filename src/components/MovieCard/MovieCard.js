7e337718ec98f1dccfc674560205001102e3e599
James Logue <logueja@gmail.com>
2018-02-10 14:48:17 -0700

    Add individual movie pages

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const MovieCard = ({ movie, onFavorite }) => {
-  const { title, overview, poster_path, backdrop_path } = movie
+  const { title, overview, poster_path, backdrop_path, movie_id } = movie
   return (
     <article className='movie-card'>
       <p className='movie-title'>{ title }</p>
       <p className='movie-overview'>{ overview }</p>
-      <img
-        className='movie-poster' src={`https://image.tmdb.org/t/p/w500${poster_path}`}/>
+      <Link to={`/movies/${movie_id}`}>
+        <img
+          className='movie-poster' src={`https://image.tmdb.org/t/p/w500${poster_path}`}/>
+      </Link>
       <img
         className='movie-backdrop' src={`https://image.tmdb.org/t/p/w500${backdrop_path}`}/>
       <button 
