90f0e277dbe045fdaa0fe970293f639af59051cc
Amanda Tjan <amanda.tjan@gmail.com>
2018-02-10 13:08:37 -0700

    Setup stubs for MovieCard test

--- src/components/MovieCard/MovieCard.test.js
+++ src/components/MovieCard/MovieCard.test.js
@@ -7,4 +7,25 @@ describe('MovieCard', () => {
   it.skip('should not pass', () => {
     expect(false).toEqual(true);
   })
+
+  let mockMovie;
+  let mockOnFavorite;
+  let renderedComponent;
+
+  beforeAll(() => {
+    mockMovie = {
+      title
+    };
+    mockOnFavorite = jest.fn();
+  })
+
+  beforeEach(() => {
+    renderedComponent = shallow(<MovieCard key={1} movie={mockMovie} onFavorite={mockOnFavorite} />)
+  })
+
+  it('should match snapshot', () => {
+
+  })
+
+  it('should call onFavorite when button is clicked')
 })
