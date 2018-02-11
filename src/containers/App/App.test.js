c65ac6fc6fc5dd225143605c7e34a7b78b6d101f
James Logue <logueja@gmail.com>
2018-02-10 18:02:13 -0700

    Add snapshot to App

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -3,8 +3,9 @@ import { shallow } from 'enzyme';
 import App from './App';
 
 describe('App', () => {
-  it.skip('should not pass', () => {
-    expect(false).toEqual(true);
+  it('should match snapshot', () => {
+    const renderedComponent = shallow(<App />)
+    expect(renderedComponent).toMatchSnapshot()
   })
 })
 
