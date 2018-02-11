55e10305c8fa3e5622ef3e4ea27ffccc81203e5c
James Logue <logueja@gmail.com>
2018-02-11 10:36:11 -0700

    Add MovieView tests

---
+++ src/components/MovieView/MovieView.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import MovieView from './MovieView';
+
+describe('MovieView', () => {
+
+  let mockMovie;
+  let mockOnFavorite;
+  let renderedComponent;
+
+  beforeAll(() => {
+    mockMovie = {
+      backdrop_path: "/askg3SMvhqEl4OL52YuvdtY40Yb.jpg",
+      movie_id: 354912,
+      overview: "Despite his family’s baffling generations-old ban on music, Miguel dreams of becoming an accomplished musician like his idol, Ernesto de la Cruz.",
+      poster_path: "/eKi8dIrr8voobbaGzDpe8w0PVbC.jpg",
+      release_date: "2017-10-27",
+      title: "Coco",
+      vote_average: 7.7
+    };
+    mockOnFavorite = jest.fn();
+  })
+
+  beforeEach(() => {
+    renderedComponent = shallow(<MovieView key={1} movie={mockMovie} onFavorite={mockOnFavorite} />)
+  })
+
+  it('should match snapshot', () => {
+    expect(renderedComponent).toMatchSnapshot();
+  })
+
+  it('should call onFavorite when button is clicked', () => {
+    renderedComponent.find('button').simulate('click');
+    expect(mockOnFavorite.mock.calls.length).toEqual(1);
+  })
+})
