de9247eda0a0e6011a9d7b677971db0da0c0f892
Jorge Ed PerezGa - Turing <perezga@icloud.com>
2018-02-11 19:51:44 -0700

    eslint helper

--- src/reducers/userReducer.test.js
+++ src/reducers/userReducer.test.js
@@ -2,7 +2,7 @@ import { userReducer } from './userReducer';
 import * as actions from '../actions';
 
 describe('userReducer', () => {
-  let user
+  let user;
 
   beforeAll(() => {
     user = {
@@ -10,25 +10,24 @@ describe('userReducer', () => {
       id: 2,
       name: "ted",
       password: "pw123"
-    }
-  })
+    };
+  });
 
   it('should return the default state', () => {
-    const expected = {}
-    expect(userReducer(undefined, {})).toEqual(expected)
-  })
+    const expected = {};
+    expect(userReducer(undefined, {})).toEqual(expected);
+  });
 
   it('ADD_USER should return the state with the user added', () => {
-    const action = actions.addUser(user)
-    const expected = user
-    expect(userReducer(undefined, action)).toEqual(expected)
-  })
+    const action = actions.addUser(user);
+    const expected = user;
+    expect(userReducer(undefined, action)).toEqual(expected);
+  });
 
   it('LOGOUT_USER should return the state with the user removed', () => {
-    const action = actions.logoutUser(user)
-    const state = user
-    const expected = {}
-    expect(userReducer(state, action)).toEqual(expected)
-  })
-
-})
+    const action = actions.logoutUser(user);
+    const state = user;
+    const expected = {};
+    expect(userReducer(state, action)).toEqual(expected);
+  });
+});
