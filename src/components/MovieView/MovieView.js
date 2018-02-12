6bf834b735819f556d16ffed9b13514109924547
James Logue <logueja@gmail.com>
2018-02-11 20:30:55 -0700

    Remove button, update tests

--- src/components/MovieView/MovieView.js
+++ src/components/MovieView/MovieView.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './MovieView.css';
 
-const MovieView = ({ movie, onFavorite }) => {
+const MovieView = ({ movie }) => {
   const { title, overview, poster_path, backdrop_path } = movie;
   return (
     <article className='movie-view'>
@@ -18,16 +18,12 @@ const MovieView = ({ movie, onFavorite }) => {
       </Link>
       <img
         className='movie-backdrop' src={`https://image.tmdb.org/t/p/w500${backdrop_path}`}/>
-      <button
-        onClick={()=> onFavorite(movie)}
-        className='favorite-button-movie-view'></button>
     </article>
   );
 };
 
 MovieView.propTypes = {
   movie: PropTypes.object,
-  onFavorite: PropTypes.func
 };
 
 export default MovieView;
